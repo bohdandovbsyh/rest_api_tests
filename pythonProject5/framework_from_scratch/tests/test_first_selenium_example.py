2665e92558fceae320ddb2b455ccbc7d2b2027ff
bohdan <dovbysh.bohdan@gmail.com>
2022-11-22 18:20:36 +0200

    Delete pythonProject5 directory

--- pythonProject5/framework_from_scratch/tests/test_first_selenium_example.py
+++
@@ -1,4 +0,0 @@
-def test_01(open_login_page):
-    login_page = open_login_page
-    dashboard_page = login_page.login('admin@yourstore.com', 'admin')
-    assert dashboard_page.is_logout_button_displayed() is True, 'User was not logged-in'
