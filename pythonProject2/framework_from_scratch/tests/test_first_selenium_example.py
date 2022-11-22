657b5c7b322d467e3f97f78eeb4bce92faf2c9b8
bohdan <dovbysh.bohdan@gmail.com>
2022-11-22 18:20:27 +0200

    Delete pythonProject2/framework_from_scratch directory

--- pythonProject2/framework_from_scratch/tests/test_first_selenium_example.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.first_test
-def test_01(open_login_page):
-    login_page = open_login_page
-    dashboard_page = login_page.login('admin@yourstore.com', 'admin')
-    assert dashboard_page.is_logout_button_displayed() is True, 'User was not logged-in'
